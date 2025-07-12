0ca184b56b54495288b4773878e1c72749e5ea42
nifethedev <nifethedev@gmail.com>
2025-07-12 18:21:38 +0100

    made the category properly centerd

--- src/app/(store)/categories/[slug]/page.tsx
+++ src/app/(store)/categories/[slug]/page.tsx
@@ -11,7 +11,7 @@ const Categories = async ({
   const products = await getProductsByCategory(slug);
   const categories = await getAllCategories();
   return (
-    <div className="flesx flex-col items-center justify-center min-h-screen justify-top bg-gray-100 p-4">
+    <div className="flex flex-col items-center justify-center min-h-screen justify-top bg-gray-100 p-4">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
         <h1 className="text-3xl font-bold mb-6 text-center">
           {slug
