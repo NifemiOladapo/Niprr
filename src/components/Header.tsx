4be717970cf2beb30b21a12094383d6298b9218a
nifethedev <nifethedev@gmail.com>
2025-07-14 16:25:52 +0100

    made header properly responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,68 +27,66 @@ const Header = () => {
     }
   };
   return (
-    <header className="flex flex-wrap justify-between items-center p-4 py-2">
-      <div className="flex justify-between w-full flex-wrap items-center">
+    <header className="flex w-full gap-3 lg:flex-row justify-between flex-col py-2 px-3 items-center">
+      <Link
+        href="/"
+        className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer"
+      >
+        Nippr
+      </Link>
+      <Form
+        action={"/search"}
+        className="mt-1 md:mt-0 flex-1 w-full text-center lg:text-left"
+      >
+        <input
+          type="text"
+          name="query"
+          placeholder="Search for products"
+          className="bg-gray-100 text-gray-800 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 border w-full max-w-4xl"
+        />
+      </Form>
+      <div className="space-x-4 mt-3.5 md:mt-0 items-center flex flex-wrap justify-center lg:space-y-0 space-y-1.5 ">
         <Link
-          href="/"
-          className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
+          href="/basket"
+          className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
-          Nipr
+          <TrolleyIcon className="w-6 h-6" />
+          <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
+            {itemCount}
+          </span>
+          <span>My Basket</span>
         </Link>
-        <Form
-          action={"/search"}
-          className="w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0"
-        >
-          <input
-            type="text"
-            name="query"
-            placeholder="Search for products"
-            className="bg-gray-100 text-gray-800 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 border w-full max-w-4xl"
-          />
-        </Form>
-        <div className="flex items-center flex-wrap space-x-4 mt-4 flex-1 sm:mt-0 sm:flex-none">
-          <Link
-            href="/basket"
-            className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            <TrolleyIcon className="w-6 h-6" />
-            <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
-              {itemCount}
-            </span>
-            <span>My Basket</span>
-          </Link>
-          <ClerkLoaded>
-            <SignedIn>
-              <Link
-                href={"/orders"}
-                className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              >
-                <PackageIcon className="w-6 h-6" />
-                <span>My Orders</span>
-              </Link>
-            </SignedIn>
+        <ClerkLoaded>
+          <SignedIn>
+            <Link
+              href={"/orders"}
+              className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              <PackageIcon className="w-6 h-6" />
+              <span>My Orders</span>
+            </Link>
+          </SignedIn>
 
-            {user ? (
-              <div className="flex items-center space-x-2">
-                <UserButton />
-                <div className="hidden sm:block text-xs">
-                  <p className="text-gray-400">Welcome Back</p>
-                  <p className="font-bold">{user?.fullName}!</p>
-                </div>
+          {user ? (
+            <div className="flex items-center space-x-2">
+              <UserButton />
+              <div className="hidden 2xl:block text-xs">
+                <p className="text-gray-400">Welcome Back</p>
+                <p className="font-bold">{user?.fullName}!</p>
               </div>
-            ) : (
-              <SignInButton mode="modal" />
-            )}
-             {user?.passkeys.length === 0 && (
-              <button
-                onClick={createClerkPasskey}
-                className="bg-white cursor-pointer hover:bg-blue-700 hover:text-white animate-pulse text-blue-500 font-bold py-2 px-4 rounded border-blue-300 border"
-              >
-                Create a passkey now
-              </button>
-            )} 
-          </ClerkLoaded>
-        </div>
+            </div>
+          ) : (
+            <SignInButton mode="modal" />
+          )}
+          {user?.passkeys.length === 0 && (
+            <button
+              onClick={createClerkPasskey}
+              className="bg-white cursor-pointer hover:bg-blue-700 hover:text-white animate-pulse text-blue-500 font-bold py-2 px-4 rounded border-blue-300 border"
+            >
+              Create a passkey now
+            </button>
+          )}
+        </ClerkLoaded>
       </div>
     </header>
   );
